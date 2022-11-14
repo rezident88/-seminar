b7bab674295298d61ba0cfb519330932d144b744
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 18:12:07 +0700

    Задача 3

---
+++ Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+if(numDay == 1) Console.WriteLine("Понедельник");
+
+else if(numDay == 2)Console.WriteLine("Вторник");
+
+else if(numDay == 3)Console.WriteLine("Среда");
+
+else if(numDay == 4)Console.WriteLine("Четверг");
+
+else if(numDay == 5)Console.WriteLine("Пятница");
+
+else if(numDay == 6)Console.WriteLine("Суббота");
+
+else if(numDay == 7)Console.WriteLine("Воскресенье");
+
+else Console.WriteLine("Ну существует");

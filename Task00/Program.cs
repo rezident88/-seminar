6f36173496831cb96590b01301cdc15f4a38f3ac
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 17:22:19 +0700

    Квадрат числа

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Конвертация текста в число
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");

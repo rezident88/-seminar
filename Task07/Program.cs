7b0e116575c1a646db4981a394c3f94561336887
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 18:27:20 +0700

    задача 7

---
+++ Task07/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int last = number % 10;// Остаток от деления 
+Console.WriteLine($"Последняя цифра заданного числа: {last} ");

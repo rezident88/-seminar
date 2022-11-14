2cd4763625d2116f5a0c12dbaa70201c628f2986
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 19:01:55 +0700

    Задача 2

---
+++ Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numA > numB)
+{
+    Console.WriteLine($"Наибольшее число  {numA}");
+    Console.WriteLine($"Наименьшее число  {numB}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число  {numB}");
+    Console.WriteLine($"Наименьшее число  {numA}");
+}

bfb92a269c416f1affb10eeb0a313862e0d6547b
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 17:56:32 +0700

    Задача 1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a == b * b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

1fed0bb487b525df2e4903c6afb3ac746eb26be8
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 19:29:25 +0700

    Задача 8

---
+++ Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+bool not = true;
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+if (i % 2 != 1)
+{
+    Console.Write(i + ", ");
+    not = false;
+}
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
+            
+    

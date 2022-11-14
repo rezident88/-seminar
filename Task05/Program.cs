30ba22fcfe3d9266d349464c64863c34e34919d2
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 18:40:19 +0700

    Задача 5

---
+++ Task05/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+if(number >= 0)
+{
+    while (count <= number)
+{
+    Console.WriteLine(count);
+    count++;
+}
+}
+else
+{
+    while (count >= number)
+    {
+        Console.WriteLine(count);
+        count--;
+    }
+}

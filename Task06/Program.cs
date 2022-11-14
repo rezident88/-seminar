dc921115dfed577371c3a87f0caafb9a21b1cee2
Ivan Kruchkov <rizident88@gmail.com>
2022-11-14 19:17:03 +0700

    Задача 6

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 1)
+{
+    Console.WriteLine($"{num} Нечетное");
+}
+else
+{
+    Console.WriteLine($"{num} Четное");
+}

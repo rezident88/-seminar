7daaf017250e5e7d3e6f1d94df438e651a3b0f64
Ivan Kruchkov <rizident88@gmail.com>
2022-11-17 16:19:30 +0700

    Работа с методом

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,12 +1,23 @@
-﻿int number = new Random().Next(10, 100); // Выводит случайное число из отрезка 10, 99
+﻿int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 --> {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if(firstDigit > secondDigit)
+
+// int firstDigit = number / 10; // 7
+// int secondDigit = number % 10; // 8
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+
+int MaxDigit(int num) // int num = number
 {
-    Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
+    int firstDigit = num / 10; // 7
+    int secondDigit = num % 10; // 8
+    // if (firstDigit > secondDigit)
+    //     return firstDigit;
+    //     return secondDigit;
+
+    return  firstDigit > secondDigit ? firstDigit : secondDigit; //если условие выполняется то выдает результат переменной после вопроса если не выполняется то послу двоеточия
+    
 }
-else
-{
-    Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
-}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа --> {maxDigit}");

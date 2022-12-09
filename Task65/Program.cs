e7a01b31b9a03d7baf14387b2b1cc1d3e2c5ee37
Ivan Kruchkov <rizident88@gmail.com>
2022-12-09 15:15:23 +0700

    Задача 65

---
+++ Task65/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите первое натуральное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void NumbersMTon(int numb1, int numb2)
+{
+    if (numb1 > numb2)
+    {
+        Console.Write($"{numb2} ");
+        NumbersMTon(numb1 , numb2 + 1);
+    }
+    if (numb1 < numb2)
+    {
+        NumbersMTon(numb1 + 1 , numb2);
+        Console.Write($"{numb1} ");
+    }
+    if (numb1 == numb2)
+    Console.Write($"{numb1} ");
+}
+
+NumbersMTon(numberM, numberN);
